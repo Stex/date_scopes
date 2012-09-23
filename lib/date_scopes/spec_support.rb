509f3e2e785e483098ee3fcc4dab2e2a5a272850
Peter Hoeg <peter@speartail.com>
2012-09-23 23:08:12 +0800

    use instance variables instead in the specs so they now can be used properly

--- lib/date_scopes/spec_support.rb
+++ lib/date_scopes/spec_support.rb
@@ -3,34 +3,35 @@ shared_examples_for 'shared date scopes' do
   before :each do
     @count = 4
     @count.times { FactoryGirl.create kind.name.underscore.to_sym }
+    @old_item = kind.first
+    @new_item = kind.last
   end
-
-  let(:old_item) { kind.first }
-  let(:new_item) { kind.last }
 
   it "should have 4 items" do
     kind.count.should eq(4)
   end
 
   it 'should support last_year, this_year and next_year' do
-    old_item.update_attribute :created_at, Date.today - 1.years
-    new_item.update_attribute :created_at, Date.today + 1.years
+    kind.all.each { |i| p "#{i.id} #{i.created_at}" }
+    @old_item.update_attribute :created_at, Time.now - 1.years
+    @new_item.update_attribute :created_at, Time.now + 1.years
+    kind.all.each { |i| p "#{i.id} #{i.created_at}" }
     kind.last_year.count.should eq(1)
     kind.this_year.count.should eq(2)
     kind.next_year.count.should eq(1)
   end
 
   it 'should support last_month, this_month and next_month' do
-    old_item.update_attribute :created_at, Date.today - 1.months
-    new_item.update_attribute :created_at, Date.today + 1.months
+    @old_item.update_attribute :created_at, Time.now - 1.months
+    @new_item.update_attribute :created_at, Time.now + 1.months
     kind.last_month.count.should eq(1)
     kind.this_month.count.should eq(2)
     kind.next_month.count.should eq(1)
   end
 
   it 'should support last_week, this_week and next_week' do
-    old_item.update_attribute :created_at, Date.today - 1.weeks
-    new_item.update_attribute :created_at, Date.today + 1.weeks
+    @old_item.update_attribute :created_at, Time.now - 1.weeks
+    @new_item.update_attribute :created_at, Time.now + 1.weeks
     kind.last_week.count.should eq(1)
     kind.this_week.count.should eq(2)
     kind.next_week.count.should eq(1)
@@ -41,7 +42,7 @@ shared_examples_for 'shared date scopes' do
     let(:date) { Date.today - 3.years }
 
     before :each do
-      old_item.update_attribute :created_at, date
+      @old_item.update_attribute :created_at, date
     end
 
     it 'should support in_year_of' do
