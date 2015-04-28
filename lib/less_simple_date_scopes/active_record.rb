5ae698b55873c04f6daae5ce622aee0c0da9f12a
Stefan Exner <stex@sterex.de>
2015-04-28 16:47:35 +0200

    Using Time.zone.now instead of Date.today to support custom time zones

--- lib/less_simple_date_scopes/active_record.rb
+++ lib/less_simple_date_scopes/active_record.rb
@@ -32,29 +32,29 @@ module LessSimpleDateScopes
         table_name = options.delete(:table_name) || self.table_name
         prefix     = options.delete(:prefix)
 
-        named_scope acts_as_date_scope_name(field, :yesterday, prefix),   lambda { in_x_of(table_name, Date.today - 1.day, field, :day, options) }
-        named_scope acts_as_date_scope_name(field, :today, prefix),       lambda { in_x_of(table_name, Date.today, field, :day, options) }
-        named_scope acts_as_date_scope_name(field, :tomorrow, prefix),    lambda { in_x_of(table_name, Date.today + 1.day, field, :day, options) }
+        named_scope acts_as_date_scope_name(field, :yesterday, prefix),   lambda { in_x_of(table_name, Time.zone.now - 1.day, field, :day, options) }
+        named_scope acts_as_date_scope_name(field, :today, prefix),       lambda { in_x_of(table_name, Time.zone.now, field, :day, options) }
+        named_scope acts_as_date_scope_name(field, :tomorrow, prefix),    lambda { in_x_of(table_name, Time.zone.now + 1.day, field, :day, options) }
 
         named_scope acts_as_date_scope_name(field, :in_week_of, prefix),  lambda {|date| in_x_of(table_name, date, field, :week, options) }
-        named_scope acts_as_date_scope_name(field, :last_week, prefix),   lambda { in_x_of(table_name, Date.today - 7.days, field, :week, options) }
-        named_scope acts_as_date_scope_name(field, :this_week, prefix),   lambda { in_x_of(table_name, Date.today, field, :week, options) }
-        named_scope acts_as_date_scope_name(field, :next_week, prefix),   lambda { in_x_of(table_name, Date.today + 7.days, field, :week, options) }
+        named_scope acts_as_date_scope_name(field, :last_week, prefix),   lambda { in_x_of(table_name, Time.zone.now - 7.days, field, :week, options) }
+        named_scope acts_as_date_scope_name(field, :this_week, prefix),   lambda { in_x_of(table_name, Time.zone.now, field, :week, options) }
+        named_scope acts_as_date_scope_name(field, :next_week, prefix),   lambda { in_x_of(table_name, Time.zone.now + 7.days, field, :week, options) }
 
         named_scope acts_as_date_scope_name(field, :in_month_of, prefix), lambda {|date| in_x_of(table_name, date, field, :month, options) }
-        named_scope acts_as_date_scope_name(field, :last_month, prefix),  lambda { in_x_of(table_name, Date.today - 1.months, field, :month, options) }
-        named_scope acts_as_date_scope_name(field, :this_month, prefix),  lambda { in_x_of(table_name, Date.today, field, :month, options) }
-        named_scope acts_as_date_scope_name(field, :next_month, prefix),  lambda { in_x_of(table_name, Date.today + 1.months, field, :month, options) }
+        named_scope acts_as_date_scope_name(field, :last_month, prefix),  lambda { in_x_of(table_name, Time.zone.now - 1.month, field, :month, options) }
+        named_scope acts_as_date_scope_name(field, :this_month, prefix),  lambda { in_x_of(table_name, Time.zone.now, field, :month, options) }
+        named_scope acts_as_date_scope_name(field, :next_month, prefix),  lambda { in_x_of(table_name, Time.zone.now + 1.month, field, :month, options) }
 
         named_scope acts_as_date_scope_name(field, :in_year_of, prefix),  lambda {|date| in_x_of(table_name, date, field, :year, options) }
-        named_scope acts_as_date_scope_name(field, :last_year, prefix),   lambda { in_x_of(table_name, Date.today - 1.years, field, :year, options) }
-        named_scope acts_as_date_scope_name(field, :this_year, prefix),   lambda { in_x_of(table_name, Date.today, field, :year, options) }
-        named_scope acts_as_date_scope_name(field, :next_year, prefix),   lambda { in_x_of(table_name, Date.today + 1.years, field, :year, options) }
+        named_scope acts_as_date_scope_name(field, :last_year, prefix),   lambda { in_x_of(table_name, Time.zone.now - 1.year, field, :year, options) }
+        named_scope acts_as_date_scope_name(field, :this_year, prefix),   lambda { in_x_of(table_name, Time.zone.now, field, :year, options) }
+        named_scope acts_as_date_scope_name(field, :next_year, prefix),   lambda { in_x_of(table_name, Time.zone.now + 1.year, field, :year, options) }
 
         named_scope acts_as_date_scope_name(field, :between, prefix),
                     lambda {|s, e|
-                      start_date = (s.is_a?(String) ? Date.parse(s) : s).beginning_of_day
-                      end_date   = (e.is_a?(String) ? Date.parse(e) : e).end_of_day
+                      start_date = (s.is_a?(String) ? Time.zone.parse(s) : s).beginning_of_day
+                      end_date   = (e.is_a?(String) ? Time.zone.parse(e) : e).end_of_day
                       options.merge({:conditions => {table_name => {field => (start_date..end_date)}}})
                     }
       end
